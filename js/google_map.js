1404b809ecda9a2f1daf305957d805e40fb4f934
PietVlem <pieter.vleminckx@student.arteveldehs.be>
2016-12-02 23:30:49 +0100

    update

--- js/google_map.js
+++ js/google_map.js
@@ -5,12 +5,19 @@
 
 (function(){
 
+    /*
+    var Link_hondenvoorzieningen = 'https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson';
+    */
+
+
+
     var mapOptions = {
         center: new google.maps.LatLng(51.035556, 3.710556),
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
 
 
     var markerOptions = {
@@ -30,6 +37,7 @@
         infoWindow.open(map, marker);
 
     });
+
 
 })();
 
