e2b25e38f1fca4a85e70615f2ec31a7cf08be6f0
PietVlem <pieter.vleminckx@student.arteveldehs.be>
2016-12-02 19:15:51 +0100

    test

--- js/google_map.js
+++ js/google_map.js
@@ -5,14 +5,8 @@
 
 (function(){
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoicGlldHZsZW0iLCJhIjoiNjE5MjlmMDAwYzI1YTUzNDQzMWU5M2I5ZTJhZmFlYmIifQ.15Xi84zS8LM84h1HvPMEsQ';
-    var map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-        center: [3.710556, 51.035556], // starting position
-        zoom: 16 // starting zoom
-    });
-/*
+
+
     var mapOptions = {
         center: new google.maps.LatLng(51.035556, 3.710556),
         zoom: 15,
@@ -46,8 +40,8 @@
 
     });
 
-    // map.data.loadGeoJson('https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson')
-*/
+    map.data.loadGeoJson('https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson')
+
 })();
 
 
